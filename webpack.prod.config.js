8899211744c35987f5506cb29c29d6815210ddac
cheng <cheng.guo@gmail.com>
2015-07-31 18:00:36 +0800

    add minify to webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,16 +1,16 @@
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var Clean = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         app: path.resolve(__dirname, 'app/src/index.js'),
-        vendors: ['react', 'flux', 'object-assign', 'keymirror'],
-        html: path.resolve(__dirname, 'build/index.html')
+        vendors: ['react', 'flux', 'object-assign', 'keymirror']
     },
     output: {
-        path: path.resolve(__dirname, 'dist', 'css'),
+        path: path.resolve(__dirname, 'dist'),
         filename: '../js/[name].js'
     },
     module: {
@@ -21,15 +21,15 @@ module.exports = {
         },
         {
           test: /\.css$/,
-          loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+          loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         },
         {
           test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff'
         },
         {
           test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: "file-loader"
+          loader: 'file-loader?[path][name].[ext]'
         }]
     },
     resolve: {
@@ -37,7 +37,21 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin('vendors', '../js/vendors.js'),
-        new ExtractTextPlugin('all.css', {allChunks: true}),
-        new Clean(['dist'])
+        new ExtractTextPlugin(
+            './css/all.css',
+            {allChunks: true}),
+        new Clean(['dist']),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            },
+            mangle: false
+        }),
+        new HtmlWebpackPlugin()
     ]
 };
